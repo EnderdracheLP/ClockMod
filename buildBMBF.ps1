a72d5de517e220a3440cfb8c21e2d8927b9653b9
BuildTools <unconfigured@null.spigotmc.org>
2021-02-05 17:23:00 +0100

    Added Cover.png Image to Mod

--- buildBMBF.ps1
+++ buildBMBF.ps1
@@ -2,5 +2,5 @@
 & $PSScriptRoot/build.ps1
 
 if ($?) {
-    Compress-Archive -Path "./libs/arm64-v8a/libclockmod.so", "./libs/arm64-v8a/libbeatsaber-hook_1_0_12.so", "./bmbfmod.json" -DestinationPath "./clockmod_v0.1.2.zip" -Update
+    Compress-Archive -Path "./libs/arm64-v8a/libclockmod.so", "./libs/arm64-v8a/libbeatsaber-hook_1_0_12.so", "./bmbfmod.json","./Cover.png" -DestinationPath "./Clockmod.zip" -Update
 }
