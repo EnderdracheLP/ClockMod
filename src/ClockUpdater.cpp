e8b460e0bac5e969c2cba22f796d533bb97c45f6
EnderdracheLP <enderdrachelp@gmail.com>
2021-02-07 23:28:25 +0100

    Tweaked Clock Position especially for the Multiplayer Lobby

--- src/ClockUpdater.cpp
+++ src/ClockUpdater.cpp
@@ -6,6 +6,7 @@
 #include "TMPro/TextMeshProUGUI.hpp"
 #include <ctime>
 #include "GlobalNamespace/OVRPlugin_OVRP_1_1_0.hpp"
+#include "UnityEngine/Vector3.hpp"
 using namespace UnityEngine;
 using namespace TMPro;
 
@@ -20,8 +21,9 @@ void ClockMod::ClockUpdater::Update() {
     time(&rawtime);
     timeinfo = localtime(&rawtime);
 
-// Checks config Settings for 12/24 Hour time and if Show Seconds is toggled on or off.
+//    MAKE_HOOK_OFFSETLESS(EnteredSettings, void, Il2CppObject * self, bool firstActivation, int activationType)
 
+// Checks config Settings for 12/24 Hour time and if Show Seconds is toggled on or off.
     if (getConfig().config["12Toggle"].GetBool() == false) {
         if (getConfig().config["SecToggle"].GetBool() == true) {    //Check if seconds should be shown
             strftime(timestr, 20, "%H:%M:%S", timeinfo);
@@ -42,19 +44,22 @@ void ClockMod::ClockUpdater::Update() {
     float fontsize = getConfig().config["FontSize"].GetFloat();
        auto text = get_gameObject()->GetComponent<TextMeshProUGUI*>();
        text->set_fontSize(fontsize);
+
        // Sets position
 //       text->get_transform()->set_position(UnityEngine::Vector3(0, 1, 2.6));
-        // Get current Battery Level
+       // Adds current Battery Level next to the clock
        if (getConfig().config["BattToggle"].GetBool() == true) {
            float batterylvl = GlobalNamespace::OVRPlugin::OVRP_1_1_0::ovrp_GetSystemBatteryLevel();
            batterylvl = batterylvl * 100;
            std::string tandb = timestr;
+
+//           tandb += " \n ";
            tandb += " - ";
+
            std::string batterylevel = std::to_string(batterylvl);
            batterylevel.erase(batterylevel.find_last_not_of('0') + 1, std::string::npos);
            batterylevel.erase(batterylevel.find_last_not_of('.') + 1, std::string::npos);
-           tandb += batterylevel;
-           tandb += "%";
+           tandb += batterylevel + "%";
 
            text->set_text(il2cpp_utils::createcsstr(tandb));
        }
--- src/ClockViewContoller.cpp
+++ src/ClockViewContoller.cpp
@@ -29,7 +29,15 @@ getConfig().config["FontSize"].SetFloat(newValue);
 void BattToggle(ClockMod::ClockViewController* parent, bool newValue) {
     getConfig().config["BattToggle"].SetBool(newValue);
 }
-
+//void SetClockXOffset(ClockMod::ClockViewController* parent, float newValue) {
+//getConfig().config["ClockXOffset"].SetFloat(newValue);
+//}
+//void SetClockYOffset(ClockMod::ClockViewController* parent, float newValue) {
+//getConfig().config["ClockYOffset"].SetFloat(newValue);
+//}
+//void SetClockZOffset(ClockMod::ClockViewController* parent, float newValue) {
+//getConfig().config["ClockZOffset"].SetFloat(newValue);
+//}
 void ClockMod::ClockViewController::DidActivate(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling) {
     if (firstActivation) {
 
@@ -40,30 +48,45 @@ void ClockMod::ClockViewController::DidActivate(bool firstActivation, bool added
             classof(UnityEngine::Events::UnityAction_1<bool>*), this, InSongToggle
             );
         UnityEngine::UI::Toggle* InsongToggleObject = QuestUI::BeatSaberUI::CreateToggle(container->get_transform(), "Show During Song", getConfig().config["insong"].GetBool(), onInSongSettingChange);
-        QuestUI::BeatSaberUI::AddHoverHint(InsongToggleObject->get_gameObject(), "Whether or not to show the Clock while playing a Beatmap.");
+        QuestUI::BeatSaberUI::AddHoverHint(InsongToggleObject->get_gameObject(), "If the Clock should be shown while playing a Beatmap.");
         // Change format 24/12
         auto onFormatSettingChange = il2cpp_utils::MakeDelegate<UnityEngine::Events::UnityAction_1<bool>*>(
             classof(UnityEngine::Events::UnityAction_1<bool>*), this, FormatToggle
             );
         UnityEngine::UI::Toggle* FormatToggleObject = QuestUI::BeatSaberUI::CreateToggle(container->get_transform(), "24/12 Toggle", getConfig().config["12Toggle"].GetBool(), onFormatSettingChange);
-        QuestUI::BeatSaberUI::AddHoverHint(FormatToggleObject->get_gameObject(), "Whether to show time in 24 Hour or 12 Hour format.");
+        QuestUI::BeatSaberUI::AddHoverHint(FormatToggleObject->get_gameObject(), "If time should be in 12 or 24 Hour format.");
         // Show Seconds
         auto onSecSettingChange = il2cpp_utils::MakeDelegate<UnityEngine::Events::UnityAction_1<bool>*>(
             classof(UnityEngine::Events::UnityAction_1<bool>*), this, SecToggle
             );
         UnityEngine::UI::Toggle* SecToggleObject = QuestUI::BeatSaberUI::CreateToggle(container->get_transform(), "Show Seconds", getConfig().config["SecToggle"].GetBool(), onSecSettingChange);
-        QuestUI::BeatSaberUI::AddHoverHint(SecToggleObject->get_gameObject(), "Whether or not to show seconds.");
+        QuestUI::BeatSaberUI::AddHoverHint(SecToggleObject->get_gameObject(), "If Seconds should be displayed.");
         // Change Size
         auto onFontSizeChange = il2cpp_utils::MakeDelegate<UnityEngine::Events::UnityAction_1<float>*>(
             classof(UnityEngine::Events::UnityAction_1<float>*), this, SetFontSize);                                 //              Decimal places   Value Steps                              Min Value   Max Value
         QuestUI::IncrementSetting* FontIncrementObject = QuestUI::BeatSaberUI::CreateIncrementSetting(container->get_transform(), "Font Size", 1, 0.1, getConfig().config["FontSize"].GetFloat(), 1.0f, 8.0f, onFontSizeChange);
         QuestUI::BeatSaberUI::AddHoverHint(FontIncrementObject->get_gameObject(), "Changes the Font Size of the Clock (Default: 4)");
-        // Show Seconds
+        // Show Battery Percentage
         auto onBattSettingChange = il2cpp_utils::MakeDelegate<UnityEngine::Events::UnityAction_1<bool>*>(
             classof(UnityEngine::Events::UnityAction_1<bool>*), this, BattToggle
             );
         UnityEngine::UI::Toggle* BattToggleObject = QuestUI::BeatSaberUI::CreateToggle(container->get_transform(), "Show Battery Percentage", getConfig().config["BattToggle"].GetBool(), onBattSettingChange);
-        QuestUI::BeatSaberUI::AddHoverHint(BattToggleObject->get_gameObject(), "Displays Battery percentage next to clock.");
+        QuestUI::BeatSaberUI::AddHoverHint(BattToggleObject->get_gameObject(), "Displays Battery percentage next to the clock.");
+        // Change Pos X
+//        auto onClockXOffsetChange = il2cpp_utils::MakeDelegate<UnityEngine::Events::UnityAction_1<float>*>(
+//            classof(UnityEngine::Events::UnityAction_1<float>*), this, SetClockXOffset);                                 //              Decimal places   Value Steps                              Min Value   Max Value
+//        QuestUI::IncrementSetting* ClockXIncrementObject = QuestUI::BeatSaberUI::CreateIncrementSetting(container->get_transform(), "Clock X Offset", 1, 0.1, getConfig().config["ClockXOffset"].GetFloat(), -10.0f, 10.0f, onClockXOffsetChange);
+//        QuestUI::BeatSaberUI::AddHoverHint(ClockXIncrementObject->get_gameObject(), "Offsets the X Position of the Clock");
+        // Change Pos Y
+//        auto onClockYOffsetChange = il2cpp_utils::MakeDelegate<UnityEngine::Events::UnityAction_1<float>*>(
+//            classof(UnityEngine::Events::UnityAction_1<float>*), this, SetClockYOffset);                                 //              Decimal places   Value Steps                              Min Value   Max Value
+//        QuestUI::IncrementSetting* ClockYIncrementObject = QuestUI::BeatSaberUI::CreateIncrementSetting(container->get_transform(), "Clock Y Offset", 1, 0.1, getConfig().config["ClockYOffset"].GetFloat(), -10.0f, 10.0f, onClockYOffsetChange);
+//        QuestUI::BeatSaberUI::AddHoverHint(ClockYIncrementObject->get_gameObject(), "Offsets the Y Position of the Clock");
+                // Change Pos Z
+//        auto onClockZOffsetChange = il2cpp_utils::MakeDelegate<UnityEngine::Events::UnityAction_1<float>*>(
+//            classof(UnityEngine::Events::UnityAction_1<float>*), this, SetClockZOffset);                                 //              Decimal places   Value Steps                              Min Value   Max Value
+//        QuestUI::IncrementSetting* ClockZIncrementObject = QuestUI::BeatSaberUI::CreateIncrementSetting(container->get_transform(), "Clock Z Offset", 1, 0.1, getConfig().config["ClockZOffset"].GetFloat(), -10.0f, 10.0f, onClockZOffsetChange);
+//        QuestUI::BeatSaberUI::AddHoverHint(ClockZIncrementObject->get_gameObject(), "Offsets the Z Position of the Clock");
     }
 }
 // Write config
