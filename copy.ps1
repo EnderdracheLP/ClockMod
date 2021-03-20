1817b5c3a63e03bf4012da0023b590cdf565d798
EnderdracheLP <enderdrachelp@gmail.com>
2021-03-20 20:48:42 +0100

    Changed line endings and testing github actions again

--- copy.ps1
+++ copy.ps1
@@ -1,6 +1,10 @@
 & $PSScriptRoot/build.ps1
 if ($?) {
     adb push libs/arm64-v8a/libclockmod.so /sdcard/Android/data/com.beatgames.beatsaber/files/mods/libclockmod.so
+    adb push libs/arm64-v8a/libbeatsaber-hook_1_2_3.so /sdcard/Android/data/com.beatgames.beatsaber/files/libs/libbeatsaber-hook_1_2_3.so
+    adb push libs/arm64-v8a/libquestui.so /sdcard/Android/data/com.beatgames.beatsaber/files/libs/libquestui.so
+    adb push libs/arm64-v8a/libcodegen_0_7_0.so /sdcard/Android/data/com.beatgames.beatsaber/files/libs/libcodegen_0_7_0.so
+    adb push libs/arm64-v8a/libcustom-types.so /sdcard/Android/data/com.beatgames.beatsaber/files/libs/libcustom-types.so
     if ($?) {
         adb shell am force-stop com.beatgames.beatsaber
         adb shell am start com.beatgames.beatsaber/com.unity3d.player.UnityPlayerActivity
--- logging.ps1
+++ logging.ps1
@@ -4,7 +4,7 @@ adb logcat -T "$timestamp" main-modloader:W QuestHook[codegen`|v0.6.3]:* QuestHo
             $timestamp = Get-Date -Format "MM-dd HH:mm:ss.fff"
             adb logcat -T "$timestamp" main-modloader:W QuestHook[codegen`|v0.6.3]:* QuestHook[QuestSounds`|v0.2.1]:* QuestHook[config-utils`|v0.2.2]:* QuestHook[questui`|v0.0.1]:* QuestHook[clockmod`|v1.4.0]:* AndroidRuntime:E *:S QuestHook[UtilsLogger`|v1.1.6]:* QuestHook[UtilsLogger`|v1.1.5]:*
         }
-                if ($args[0] -eq "--insane") {
+        if ($args[0] -eq "--insane") {
             $timestamp = Get-Date -Format "MM-dd HH:mm:ss.fff"
             adb logcat -T "$timestamp" main-modloader:W QuestHook[codegen`|v0.6.3]:* QuestHook[QuestSounds`|v0.2.1]:* QuestHook[config-utils`|v0.2.2]:* QuestHook[questui`|v0.0.1]:* QuestHook[clockmod`|v1.4.0]:* AndroidRuntime:E *:S QuestHook[UtilsLogger`|v1.1.6]:* QuestHook[UtilsLogger`|v1.1.5]:*
         }
