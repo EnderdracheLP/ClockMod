642c03b0300983f8452776c9c5960860df2ee449
EnderdracheLP <enderdrachelp@gmail.com>
2021-04-30 21:04:55 +0200

    Made changes to build and action scripts

--- buildQMOD.ps1
+++ buildQMOD.ps1
@@ -2,7 +2,7 @@
 if ($args.Count -eq 0) {
 $ModID = "clockmod"
 $BSHook = "1_3_3"
-
+$BS_Version = "1.15.0"
 echo "Compiling Mod"
 & $PSScriptRoot/build.ps1
 }
@@ -12,9 +12,11 @@ echo "Compiling Mod"
 if ($args[0] -eq "--package") {
     $ModID = $env:module_id
     $BSHook = $env:bs_hook
+    $BS_Version = $env:BSVersion
 echo "Actions: Packaging QMod with ModID: $ModID and BS-Hook version: $BSHook"
     (Get-Content "./mod.json").replace('{VERSION_NUMBER_PLACEHOLDER}', "$env:version") | Set-Content "./mod.json"
     (Get-Content "./mod.json").replace('{BS_Hook}', "$BSHook") | Set-Content "./mod.json"
+    (Get-Content "./mod.json").replace('{BS_Version}', "$BS_Version") | Set-Content "./mod.json"
     Compress-Archive -Path "./libs/arm64-v8a/lib$ModID.so", "./libs/arm64-v8a/libbeatsaber-hook_$BSHook.so", ".\mod.json" -DestinationPath "./Temp$ModID.zip" -Update
     Move-Item "./Temp$ModID.zip" "./$ModID.qmod" -Force
 }
@@ -22,6 +24,7 @@ if ($? -And $args.Count -eq 0) {
 echo "Packaging QMod with ModID: $ModID"
     (Get-Content $modjson).replace('{VERSION_NUMBER_PLACEHOLDER}', "$VERSION") | Set-Content $modjson
     (Get-Content $modjson).replace('{BS_Hook}', "$BSHook") | Set-Content $modjson
+    (Get-Content "./mod.json").replace('{BS_Version}', "$BS_Version") | Set-Content "./mod.json"
     Compress-Archive -Path "./libs/arm64-v8a/lib$ModID.so", "./libs/arm64-v8a/libbeatsaber-hook_$BSHook.so", ".\mod.json" -DestinationPath "./Temp$ModID.zip" -Update
     Move-Item "./Temp$ModID.zip" "./$ModID.qmod" -Force
 }
