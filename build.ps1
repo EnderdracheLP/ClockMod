b3dedbd6e0d867456a38d1b0bbdaa9e66d7f481c
EnderdracheLP <enderdrachelp@gmail.com>
2021-05-10 14:21:07 +0200

    Updated Legacy 1.13.2 branch (For all those people sneaking around for BMBF 1.13.2 dev builds here you go)

--- build.ps1
+++ build.ps1
@@ -1,4 +1,16 @@
 $NDKPath = Get-Content $PSScriptRoot/ndkpath.txt
+if ($args.Count -eq 0) {
+$ModID = "clockmod"
+$VERSION = "1.4.0-InDev"
+}
+
+if ($args[0] -eq "--actions") {
+    $ModID = $env:module_id
+    $VERSION = $env:version
+}
+echo "Building mod with ModID: $ModID version: $VERSION"
+Copy-Item "./Android_Template.mk" "./Android.mk" -Force
+(Get-Content "./Android.mk").replace('{VERSION}',   "$VERSION")       | Set-Content "./Android.mk"
 
 $buildScript = "$NDKPath/build/ndk-build"
 if (-not ($PSVersionTable.PSEdition -eq "Core")) {
@@ -6,3 +18,5 @@ if (-not ($PSVersionTable.PSEdition -eq "Core")) {
 }
 
 & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
+
+echo Done
--- buildBMBF.ps1
+++ buildBMBF.ps1
@@ -1,7 +1,20 @@
 # Builds a .zip file for loading with BMBF
+if ($args.Count -eq 0) {
+$VERSION = "1.4.0-InDev"
 & $PSScriptRoot/build.ps1
+}
 
-if ($?) {
+    Copy-Item "./bmbfmod_Template.json" "./bmbfmod.json" -Force
+
+if ($args[0] -eq "--package") {
+echo "Actions: Packaging BMBF Mod"
+    (Get-Content "./bmbfmod.json").replace('{VERSION_NUMBER_PLACEHOLDER}', "$env:version") | Set-Content "./bmbfmod.json"
+    Compress-Archive -Path "./libs/arm64-v8a/libclockmod.so", "./libs/arm64-v8a/libbeatsaber-hook_1_0_12.so", "./bmbfmod.json","./Cover.png" -DestinationPath "./Clockmod.zip" -Update
+}
+
+if ($? -And $args.Count -eq 0) {
+    echo "Packaging BMBF Mod"
+    (Get-Content "./bmbfmod.json").replace('{VERSION_NUMBER_PLACEHOLDER}', "$env:version") | Set-Content "./bmbfmod.json"
     Compress-Archive -Path "./libs/arm64-v8a/libclockmod.so", "./libs/arm64-v8a/libbeatsaber-hook_1_0_12.so", "./bmbfmod.json","./Cover.png" -DestinationPath "./Clockmod.zip" -Update
 }
 echo "Task Completed"
