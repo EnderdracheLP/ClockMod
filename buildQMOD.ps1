545001d60803c1fb20ff839718068e04d5aafc6f
EnderdracheLP <enderdrachelp@gmail.com>
2021-07-07 20:30:52 +0200

    Fix buildQMOD

--- buildQMOD.ps1
+++ buildQMOD.ps1
@@ -1,8 +1,8 @@
 # Builds a .qmod file for loading with QP
 if ($args.Count -eq 0) {
 $ModID = "clockmod"
-$BSHook = "1_3_3"
-$BS_Version = "1.15.0"
+$BSHook = "2_0_3"
+$BS_Version = "1.16.2"
 echo "Compiling Mod"
 & $PSScriptRoot/build.ps1
 }
@@ -22,8 +22,8 @@ echo "Actions: Packaging QMod with ModID: $ModID and BS-Hook version: $BSHook"
 }
 if ($? -And $args.Count -eq 0) {
 echo "Packaging QMod with ModID: $ModID"
-    (Get-Content $modjson).replace('{VERSION_NUMBER_PLACEHOLDER}', "$VERSION") | Set-Content $modjson
-    (Get-Content $modjson).replace('{BS_Hook}', "$BSHook") | Set-Content $modjson
+    (Get-Content "./mod.json").replace('{VERSION_NUMBER_PLACEHOLDER}', "$VERSION") | Set-Content "./mod.json"
+    (Get-Content "./mod.json").replace('{BS_Hook}', "$BSHook") | Set-Content "./mod.json"
     (Get-Content "./mod.json").replace('{BS_Version}', "$BS_Version") | Set-Content "./mod.json"
     Compress-Archive -Path "./libs/arm64-v8a/lib$ModID.so", "./libs/arm64-v8a/libbeatsaber-hook_$BSHook.so", ".\mod.json" -DestinationPath "./Temp$ModID.zip" -Update
     Move-Item "./Temp$ModID.zip" "./$ModID.qmod" -Force
