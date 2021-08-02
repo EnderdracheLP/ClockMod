cef0ccfcfd420d8bbf3907f3a4cfa0d8d07f4797
EnderdracheLP <enderdrachelp@gmail.com>
2021-08-02 14:36:10 +0200

    Test, GITHUB environment NDK, fixed buildQMOD

--- Android.mk
+++ Android.mk
@@ -35,19 +35,19 @@ LOCAL_MODULE := codegen_0_12_5
 LOCAL_EXPORT_C_INCLUDES := extern/codegen
 LOCAL_SRC_FILES := extern/libcodegen_0_12_5.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: custom-types - version: 0.12.6
+# Creating prebuilt for dependency: custom-types - version: 0.8.3
 include $(CLEAR_VARS)
 LOCAL_MODULE := custom-types
 LOCAL_EXPORT_C_INCLUDES := extern/custom-types
 LOCAL_SRC_FILES := extern/libcustom-types.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: modloader - version: 1.2.3
+# Creating prebuilt for dependency: modloader - version: 1.1.0
 include $(CLEAR_VARS)
 LOCAL_MODULE := modloader
 LOCAL_EXPORT_C_INCLUDES := extern/modloader
 LOCAL_SRC_FILES := extern/libmodloader.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: questui - version: 0.10.1
+# Creating prebuilt for dependency: questui - version: 0.6.11
 include $(CLEAR_VARS)
 LOCAL_MODULE := questui
 LOCAL_EXPORT_C_INCLUDES := extern/questui
--- buildQMOD.ps1
+++ buildQMOD.ps1
@@ -1,6 +1,7 @@
 # Builds a .qmod file for loading with QP
 if ($args.Count -eq 0) {
 $ModID = "clockmod"
+$VERSION = "1.4.0"
 $BSHook = "2_2_4"
 $BS_Version = "1.16.4"
 echo "Compiling Mod"
@@ -11,17 +12,15 @@ echo "Compiling Mod"
 # TODO: Get the below working with Github Actions variables.
 if ($args[0] -eq "--package") {
     $ModID = $env:module_id
-    $BSHook = $env:bs_hook
-    $BS_Version = $env:BSVersion
 echo "Actions: Packaging QMod with ModID: $ModID and BS-Hook version: $BSHook"
     (Get-Content "./mod.json").replace('{VERSION_NUMBER_PLACEHOLDER}', "$env:version") | Set-Content "./mod.json"
-    (Get-Content "./mod.json").replace('{BS_Hook}', "$BSHook") | Set-Content "./mod.json"
-    (Get-Content "./mod.json").replace('{BS_Version}', "$BS_Version") | Set-Content "./mod.json"
+    (Get-Content "./mod.json").replace('{BS_Hook}', "$env:bs_hook") | Set-Content "./mod.json"
+    (Get-Content "./mod.json").replace('{BS_Version}', "$env:BSVersion") | Set-Content "./mod.json"
     Compress-Archive -Path "./libs/arm64-v8a/lib$ModID.so", "./libs/arm64-v8a/libbeatsaber-hook_$BSHook.so", ".\mod.json", ".\Cover.jpg" -DestinationPath "./Temp$ModID.zip" -Update
     Move-Item "./Temp$ModID.zip" "./$ModID.qmod" -Force
 }
 if ($? -And $args.Count -eq 0) {
-echo "Packaging QMod with ModID: $ModID"
+echo "Packaging QMod with ModID: $ModID and version: $VERSION"
     (Get-Content "./mod.json").replace('{VERSION_NUMBER_PLACEHOLDER}', "$VERSION") | Set-Content "./mod.json"
     (Get-Content "./mod.json").replace('{BS_Hook}', "$BSHook") | Set-Content "./mod.json"
     (Get-Content "./mod.json").replace('{BS_Version}', "$BS_Version") | Set-Content "./mod.json"
