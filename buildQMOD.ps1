23a8c9f277dd4af72e287bab0422010623a29e2e
EnderdracheLP <enderdrachelp@gmail.com>
2021-08-02 14:41:45 +0200

    Fix buildQMOD, yet again

--- buildQMOD.ps1
+++ buildQMOD.ps1
@@ -12,10 +12,12 @@ echo "Compiling Mod"
 # TODO: Get the below working with Github Actions variables.
 if ($args[0] -eq "--package") {
     $ModID = $env:module_id
+    $BSHook = $env:bs_hook
+    $BS_Version = $env:BSVersion
 echo "Actions: Packaging QMod with ModID: $ModID and BS-Hook version: $BSHook"
     (Get-Content "./mod.json").replace('{VERSION_NUMBER_PLACEHOLDER}', "$env:version") | Set-Content "./mod.json"
-    (Get-Content "./mod.json").replace('{BS_Hook}', "$env:bs_hook") | Set-Content "./mod.json"
-    (Get-Content "./mod.json").replace('{BS_Version}', "$env:BSVersion") | Set-Content "./mod.json"
+    (Get-Content "./mod.json").replace('{BS_Hook}', "$BSHook") | Set-Content "./mod.json"
+    (Get-Content "./mod.json").replace('{BS_Version}', "$BS_Version") | Set-Content "./mod.json"
     Compress-Archive -Path "./libs/arm64-v8a/lib$ModID.so", "./libs/arm64-v8a/libbeatsaber-hook_$BSHook.so", ".\mod.json", ".\Cover.jpg" -DestinationPath "./Temp$ModID.zip" -Update
     Move-Item "./Temp$ModID.zip" "./$ModID.qmod" -Force
 }
