40a73b49cdf304d849ef6d10713e8d027795e830
EnderdracheLP <enderdrachelp@elp.quest>
2024-10-07 19:43:22 +0200

    Add InReplay to config

--- include/ClockModConfig.hpp
+++ include/ClockModConfig.hpp
@@ -3,13 +3,14 @@
 
 DECLARE_CONFIG(ModConfig,
 
-    CONFIG_VALUE(InSong,           bool,   "Show During Song",                     true,   "If the Clock should be shown while playing a Beatmap.");
-    CONFIG_VALUE(TwelveToggle,     bool,   "24/12 Toggle",                         false,  "If time should be in 12 or 24 Hour format.");
-    CONFIG_VALUE(SecToggle,        bool,   "Show Seconds",                         false,  "If Seconds should be displayed.");
+    CONFIG_VALUE(InSong,           bool,   "Show During Song",                     true,   "If the Clock should be shown while playing a beatmap.");
+    CONFIG_VALUE(InReplay,           bool,   "Show During Replay",                 true,   "If the Clock should be shown while playing a replay.");
+    CONFIG_VALUE(TwelveToggle,     bool,   "24/12 Toggle",                         false,  "If time should be in 12 or 24 hour format.");
+    CONFIG_VALUE(SecToggle,        bool,   "Show Seconds",                         false,  "If seconds should be displayed.");
     CONFIG_VALUE(BattToggle,       bool,   "Show Battery Percentage",              false,  "Displays Battery percentage next to the clock.");
     CONFIG_VALUE(RainbowClock,     bool,   "Rainbowify it",                        false,  "Makes the Clock beautiful.");
     CONFIG_VALUE(FontSize,         float,  "Font Size",                            3.5,    "Changes the Font Size of the Clock (Default: 3.5)");
-    CONFIG_VALUE(ClockPosition,    bool,   "Clock Position (Top/Bottom)",          false,  "If the Clock should be at the Top or the Bottom");
+    CONFIG_VALUE(ClockPosition,    bool,   "Clock Position (Top/Bottom)",          false,  "If the Clock should be at the top or the bottom");
 
 //    CONFIG_VALUE(ClockXOffset, double, "Clock X Offset", 0);
 //    CONFIG_VALUE(ClockYOffset, double, "Clock Y Offset", 0);
